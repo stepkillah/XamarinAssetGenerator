cbde571b3a1e17c5350d175059d0f61bfe636acb
Anton Yaroshenko <kpakadijl@hotmail.com>
2019-11-01 14:06:10 +0200

    removed transparency from icons

--- AssetGeneratore/PngHelper.cs
+++ AssetGeneratore/PngHelper.cs
@@ -12,9 +12,9 @@ namespace AssetGenerator
             var svg2 = new SKSvg(new SKSize(width, height));
             svg2.Load(filepath);
             using var image = icon
-                ? ConvertProfile(SKImage.FromPicture(svg2.Picture, new SKSizeI((int) width, (int) height)), width,
+                ? ConvertProfile(SKImage.FromPicture(svg2.Picture, new SKSizeI((int)width, (int)height)), width,
                     height)
-                : SKImage.FromPicture(svg2.Picture, new SKSizeI((int) width, (int) height));
+                : SKImage.FromPicture(svg2.Picture, new SKSizeI((int)width, (int)height));
             using var data = image.Encode(SKEncodedImageFormat.Png, quality);
             // save the data to a stream
             await using var stream = File.OpenWrite(filename);
@@ -33,9 +33,33 @@ namespace AssetGenerator
 
             SKImage newImg = SKImage.Create(info);
             srcImg.ScalePixels(newImg.PeekPixels(), SKFilterQuality.None);
+
+            // Remove transparency
+            var bitmap = RemoveTransparency(srcImg);
+            newImg = SKImage.FromBitmap(bitmap);
             // now when doing this resize, Skia knows the original ColorSpace, and the
             // destination ColorSpace, and converts the colors from CMYK to sRGB.
             return newImg;
+        }
+
+        private static SKBitmap RemoveTransparency(SKImage orig)
+        {
+            var original = SKBitmap.FromImage(orig);
+
+            // create a new bitmap with the same dimensions
+            // also avoids the first copy if the color type is index8
+            var copy = new SKBitmap(original.Width, original.Height);
+
+            using (var canvas = new SKCanvas(copy))
+            {
+                // clear the bitmap with the desired color for transparency
+                canvas.Clear(SKColors.White);
+
+                // draw the bitmap on top
+                canvas.DrawBitmap(original, 0, 0);
+            }
+
+            return copy;
         }
     }
 }
